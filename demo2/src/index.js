978f9f453b1cad4c6db8ab7a4427c9fbc941a05d
corner <corner_scofield@163.com>
2020-06-09 21:04:08 +0800

    youhua

--- demo2/src/index.js
+++ demo2/src/index.js
@@ -1,7 +1,4 @@
 console.log(222)
+
 var wrap = document.getElementById('body-wrap')
 wrap.style.background = '#666'
-
-window.onload = function(){
-    console.log($('body'))
-}
--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,5 +1,4 @@
 module.exports = function(env,args){
-    console.log(env, 'env')
     return {
         mode: 'development',
         entry: ['./src/index.js', './src/header.js']
