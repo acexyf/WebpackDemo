d045a20f1281d3aaa9dea80f3c9f9563e1b2b22e
corner <corner_scofield@163.com>
2020-06-10 17:29:09 +0800

    优化

--- demo4/src/index.js
+++ demo4/src/index.js
@@ -2,4 +2,37 @@ import './index.css'
 import './left.scss'
 import './right.less'
 
-console.log('index')
+
+{
+    
+    let a = 10;
+    let [a1, b, c] = [1, 2, 3];
+    console.log(`index`)
+
+    let arrayLike = {
+        '0': 'a',
+        '1': 'b',
+        '2': 'c',
+        length: 3
+    };
+
+
+    let arr1 = Array.from(arrayLike);
+
+    ['a', 'b', 'c'].fill(7)
+
+    [1, 2, 3, 4, 5].copyWithin(0, 3, 4)
+
+
+    for (let index of ['a', 'b'].keys()) {
+        console.log(index);
+    }
+
+
+    [1, 2, 3].includes(2)
+}
+
+
+
+
+
