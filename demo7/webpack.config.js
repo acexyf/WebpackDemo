4f1d046e021d0ae54838f2e69b77ebc504260376
acexyf <corner_scofield@163.com>
2020-06-11 23:18:04 +0800

    新增plugin demo

---
+++ demo7/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                },{
+                    loader: 'css-loader'
+                },{
+                    loader: 'less-loader'
+                }]
+            },{
+                test: /\.js/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        })
+    ]
+}
