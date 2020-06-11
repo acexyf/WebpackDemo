bd5215e0fe5fa7b631bd3e3ff099a6d6634234ec
corner <corner_scofield@163.com>
2020-06-11 14:03:18 +0800

    youhua

--- demo3/webpack.config.js
+++ demo3/webpack.config.js
@@ -19,8 +19,29 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'all.html',
-            title: 'all',
-            chunks: 'all'
+            title: 'title all',
+            chunks: 'all',
+            inject: true,
+            hash: true,
+            favicon: '',
+            meta: {
+                'viewport': 'width=device-width, initial-scale=1.0'
+            },
+            minify: {
+                //清除script标签引号
+                removeAttributeQuotes: true,
+                //清除html中的注释
+                removeComments: true,
+                //清除html中的空格、换行符
+                //html压缩成一行
+                collapseWhitespace: false,
+                //压缩html内的样式
+                minifyCSS: true,
+                //清除内容为空的元素（慎用）
+                removeEmptyElements: false,
+                //清除style和link标签的type属性
+                removeStyleLinkTypeAttributes: false
+            }
         }),
         new HtmlWebpackPlugin({
             template: './index.html',
--- demo4/webpack.config.js
+++ demo4/webpack.config.js
@@ -1,5 +1,7 @@
 
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -7,6 +9,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+        })
+    ],
     module: {
         rules: [{
             test: /\.css/,
