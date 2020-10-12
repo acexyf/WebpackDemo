237169211e7b567be34b752970d087c93a29076f
corner <corner_scofield@163.com>
2020-10-12 15:25:36 +0800

    优化

--- demo19/loader/less-loader.js
+++ demo19/loader/less-loader.js
@@ -1,11 +1,19 @@
-let less = require("less")
+const less = require("less");
+const utils = require("loader-utils");
+
 function loader(source) {
+  const options = utils.getOptions(this);
+
+  const callback = this.async();
+
   let css = "";
-  console.log(source,'source')
-  less.render(source, function(err, c) {  // 这是less插件提供的解析方法
-    css = c.css;
-  })
-  // css = css.replace(/\n/g, "\\n");
-  return css;
+  console.log(options, "options");
+
+  less.render(source, function (err, c) {
+    // 这是less插件提供的解析方法
+    let { css } = c;
+    callback(null, css);
+  });
+  //   return css;
 }
 module.exports = loader;
--- demo19/webpack.config.js
+++ demo19/webpack.config.js
@@ -1,38 +1,50 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
   module: {
-    rules: [{
-      test: /\.less/,
-      use: [{
-          loader: path.resolve(__dirname, "loader", "style-loader")
-        },
-        {
-          loader: path.resolve(__dirname, "loader", "less-loader")
-        }
-      ],
-    }, {
-      test: /\.js/,
-      use: [{
-        loader: "babel-loader",
-        options: {
-          cacheDirectory: true
-        }
-      }]
-
-    }]
+    rules: [
+      {
+        test: /\.less/,
+        use: [
+          {
+            loader: path.resolve(__dirname, "loader", "style-loader"),
+            options: {
+              cache: true,
+            },
+          },
+          {
+            loader: path.resolve(__dirname, "loader", "less-loader"),
+            options: {
+              cache: true,
+              key: 'qweasd'
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    })
-  ]
-}
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
