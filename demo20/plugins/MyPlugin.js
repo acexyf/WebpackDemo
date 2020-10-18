cb78f0d30d23e0422eed94b0e8b457b7ac54723e
corner <corner_scofield@163.com>
2020-10-18 21:43:01 +0800

    youhua

--- demo20/plugins/MyPlugin.js
+++ demo20/plugins/MyPlugin.js
@@ -1,14 +1,27 @@
 class MyPlugin {
   constructor(options) {
-    console.log("constructor", options);
+    console.log("Plugin被创建了");
   }
   apply(compiler) {
-    compiler.plugin("compilation", (compilation) => {
-      console.log("compilation");
-    });
-    compiler.hooks.done.tap("MyPlugin", (stats) => {
-      console.log("done");
-    });
+    // compiler.plugin("compilation", (compilation) => {
+    //   console.log("compilation");
+    // });
+    // compiler.hooks.done.tap("MyPlugin", (stats) => {
+    //   console.log("done");
+    // });
+
+    // compiler.plugin('emit', (compilation, callback)=>{
+    //   console.log('生成资源到 output 目录之前。')
+    //   callback()
+    // })
+    // compiler.hooks.emit.tapAsync('MyPlugin', function (name, cb) {
+    //   console.log('生成资源到 output 目录之前。')
+    //   cb()
+    // })
+
+    // compiler.hooks.done.tap('MyPlugin', function (compilation) {
+    //   console.log('done')
+    // })
   }
 }
 module.exports = MyPlugin;
